9c96f74f2384748b5979e7198bafcd364a20b2dd
FUH22860 <FUH22860@spengergasse.at>
2022-12-02 21:21:45 +0100

    Shell command test Successfully executed a shell command in C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+Process process = new Process();
+
+ProcessStartInfo processStartInfo = new ProcessStartInfo();
+processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+processStartInfo.FileName = @"ls";
+processStartInfo.WorkingDirectory = @"/home/andre";
+processStartInfo.Arguments = "--color";
+processStartInfo.RedirectStandardOutput = false;
+processStartInfo.RedirectStandardError = false;
+processStartInfo.UseShellExecute = true;
+
+process.StartInfo = processStartInfo;
+process.Start();
