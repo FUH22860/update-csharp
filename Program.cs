9c05f3ac838af0bc2e07d578cbe87b9b22fd4d33
FUH22860 <FUH22860@spengergasse.at>
2022-12-05 18:08:45 +0100

    Trying to get the user home

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
 ﻿using System.Diagnostics;
 
-Process process = new Process();
+    string homePath;
+    if(Environment.OSVersion.Platform == PlatformID.Unix) {
+        homePath = Environment.GetEnvironmentVariable("HOME");
+    } else {
+        Console.WriteLine("This script doesn't support your operating system.");
+    }
 
-ProcessStartInfo processStartInfo = new ProcessStartInfo();
-processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-processStartInfo.FileName = @"ls";
-processStartInfo.WorkingDirectory = @"/home/andre";
-processStartInfo.Arguments = "--color";
-processStartInfo.RedirectStandardOutput = false;
-processStartInfo.RedirectStandardError = false;
-processStartInfo.UseShellExecute = true;
+    Process process = new Process();
 
-process.StartInfo = processStartInfo;
-process.Start();
+    ProcessStartInfo processStartInfo = new ProcessStartInfo();
+    //processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+    processStartInfo.FileName = @"yay";
+    //processStartInfo.WorkingDirectory = homePath;
+    //processStartInfo.Arguments = "--color";
+    processStartInfo.RedirectStandardOutput = false;
+    processStartInfo.RedirectStandardError = false;
+    processStartInfo.UseShellExecute = true;
+
+    process.StartInfo = processStartInfo;
+    process.Start();
